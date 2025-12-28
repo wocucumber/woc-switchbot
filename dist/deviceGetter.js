edc7baeca1fa18bd9395f83ecbbc1c5f8e7ef1f4
wocucumber <wocucumber@gmail.com>
2025-12-28 15:40:54 +0900

    Rewrite core logic in TypeScript.

---
+++ dist/deviceGetter.js
@@ -0,0 +1,6 @@
+import { SwitchbotRequester } from "./core.js";
+export default class DeviceGetter extends SwitchbotRequester {
+    async getDevices() {
+        return (await this._getRequest("1.1", "/devices")).body;
+    }
+}
