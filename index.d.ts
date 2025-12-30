a1594d6b2c070db177e7d1a057cb9f39d3b8ab6d
wocucumber <wocucumber@gmail.com>
2025-12-30 19:05:14 +0900

    Support ts

---
+++ index.d.ts
@@ -0,0 +1,5 @@
+export { Config, SwitchBotAPI } from "./dist/core.d.ts";
+export * from "./dist/physical/_physical.d.ts";
+export * as IR from "./dist/ir/_ir.d.ts";
+export {default as Scenes} from "./dist/scenes.d.ts";
+export {default as generateIDFile} from "./dist/generateIdFile.d.ts";
